92c8588ca55ca41691ac9d1d4e6735e99fffa24d
Mário Mello <mario@dindigital.com>
2014-10-23 12:06:34 -0200

    iniciando repositório

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+
+		<title></title>
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<link rel="shortcut icon" href="/favicon.ico">
+
+		<!-- FACEBOOK
+		<meta property="og:locale" content="pt_BR">
+		<meta property="og:url" content="">
+		<meta property="og:title" content="">
+		<meta property="og:site_name" content="">
+		<meta property="og:description" content="">
+		<meta property="og:image" content="">
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="=">
+		<meta property="og:image:height" content="=">
+		<meta property="og:type" content="website">
+		-->
+
+		<link rel="stylesheet" href="/site/vendor/normalize/normalize.css">
+		<link rel="stylesheet" href="/site/css/base.css">
+		<link rel="stylesheet" href="/site/css/base_responsive.css">
+
+		<script src="/site/vendor/modernizr/modernizr.js"></script>
+		<!--[if lt IE 9]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+
+		<h1>oi</h1>
+
+		<script src="/site/js/base.js"></script>
+
+	</body>
+
+</html>
