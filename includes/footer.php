a8baa3b8756924f8d00e2d1a543c83f1abed2a8e
nncl <nncl@live.com>
2015-03-25 16:52:14 -0300

    Criação de nova página e inserção de microdatas.

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,17 @@
 		
 
 		<footer itemscope itemtype="http://schema.org/WPFooter">
-			<p role="content info">Todos os direitos reservados.</p>
+			<p role="contentinfo">Todos os direitos reservados.</p>
 
 			<div class="contacts" itemscope itemtype="http://schema.org/Organization">
 				<div class="box ico-place" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
 					<div class="all-center">
 						<div class="center">
-							<h2 itemprop="streetAddress">Rua x, 111</h2>
-							<h3 itemprop="postalCode">CEP 00000-000 Bairro</h3>
-							<h3 itemprop="addressLocality">Cidade SP</h3>									
+							<address>
+								<h2 itemprop="streetAddress">Rua x, 111</h2>
+								<h3 itemprop="postalCode">CEP 00000-000 Bairro</h3>
+								<h3 itemprop="addressLocality">Cidade SP</h3>									
+							</address>								
 						</div>
 					<span class="line-space"></span>
 					</div>
@@ -23,7 +25,9 @@
 
 				<div class="all-center">
 					<div class="center">
-						<a itemprop="email" href="mailto:contato@rebrip.org.br">contato@empresa.com.br</a>
+						<address>
+							<a itemprop="email" href="mailto:contato@rebrip.org.br">contato@empresa.com.br</a>
+						</address>
 					</div>
 				</div>
 				
